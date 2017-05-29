aa667a2aa170d77d9cda3df4fa25340c600706ec
KAU <KAU@KAU-PC>
2017-05-29 17:08:33 +0900

    outer_loop 추가 예외발생 추가

--- baseball/src/baseball/BaseBall.java
+++ baseball/src/baseball/BaseBall.java
@@ -3,69 +3,67 @@ package baseball;
 import java.util.Scanner;
 
 public class BaseBall {
-	
-	int strike = 0;
-	int ball = 0;
+
+	int strike;
+	int ball;
+	int out;
 	int[] com = new int[3];
 	int[] user = new int[3];
-	
-	
-	public BaseBall(){
+
+	public BaseBall() {
 		strike = 0;
 		ball = 0;
+		out = 0;
 	}
-	
-	
-	
-	
+
 	public void makingNum() {
-		
+
 		while (true) {
 
 			for (int j = 0; j <= 2; j++) {
-				com[j] =(int)(Math.random() * 10);// random 함수는 double 반환 하기 때문에 int형으로 typecasting
-													
+				com[j] = (int) (Math.random() * 9) + 1;// typecasting
+
 			}
 			if (com[0] != com[1] && com[1] != com[2] && com[2] != com[0])
 				break;
 		}
 
 	}
-	
-	
+
 	public void userNum() throws Exception {
 		Scanner sc = new Scanner(System.in);
 
 		for (int i = 0; i < 3; i++) {
 			user[i] = sc.nextInt();
-			
-			if (user[i] < 0 || user[i] > 9) {
-				throw new Exception("0~9 사이의 숫자를 입력하세요");
+
+			if (user[i] < 1 || user[i] > 9) {
+				throw new Exception("오류발생 : 1~9 사이의 숫자를 입력하세요");
 			}
+		}
+		if (user[0] == user[1] || user[1] == user[2] || user[2] == user[0]) {
+			throw new Exception("오류발생 : 서로 다른 숫자를 입력하세요");
 		}
 
 	}
-	
-	
-	public void checkingNum(){
-		  
-			for(int i=0; i<3; i++)
-	        {
-	            for(int j=0; j<3; j++)
-	            {
-	                if((com[i] == user[j])){
-	                	if(i==j){
-	                		strike += 1;
-	                	}else{
-	                		ball += 1;
-	                	}
-	            	
-	                }
-	            }        
-	        }System.out.println("Strike /// Ball /// OUT");
-	         System.out.printf("%d        %d" ,strike,ball);
-		 }
-	
-	
-	
+
+	public void checkingNum() {
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if ((com[i] == user[j])) {
+					if (i == j) {
+						strike += 1;
+					} else {
+						ball += 1;
+					}
+
+				}
+			}
+		}
+		out += 1;
+		System.out.printf("Strike /// Ball /// OUT(%d chance left)\n", 9 - out);
+		System.out.printf("%d        %d        %d\n", strike, ball, out);
+
+	}
+
 }
--- baseball/src/baseball/Test.java
+++ baseball/src/baseball/Test.java
@@ -4,29 +4,40 @@ import java.util.InputMismatchException;
 
 public class Test {
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 		BaseBall baseball = new BaseBall();
 
-		try {
-			baseball.makingNum();
-			System.out.println(baseball.com[0]);
-			System.out.println(baseball.com[1]);
-			System.out.println(baseball.com[2]);
+		baseball.makingNum();
+		System.out.println(baseball.com[0]);
+		System.out.println(baseball.com[1]);
+		System.out.println(baseball.com[2]);
+		
+		outer_loop:
+		while (true) {
+			try {
+				while (true) {
+					System.out.println("3가지 숫자를 입력하세요");
+					baseball.userNum();
+					baseball.checkingNum();
 
-			
-			  while(true){
-			baseball.userNum(); baseball.checkingNum();
-			  if(baseball.strike ==3){ break; }
-			  
-			  
-			  }
-			 
+					if (baseball.strike == 3) {
+						System.out.println("\nYOU WIN");
+						break outer_loop;
+					} else if (baseball.out == 9) {
+						System.out.println("\nYOU LOSE");
+						break outer_loop;
+					}
+					baseball.strike = 0;
+					baseball.ball = 0;
 
-		} catch (InputMismatchException   e) {
-			System.out.println("숫자를 입력하세요");
-		} catch(Exception e){
-			System.out.println(e.toString());
+				}
+
+			} catch (InputMismatchException e) {
+				System.out.println("오류발생 : 숫자를 입력하세요");
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
 		}
-
 	}
 }
